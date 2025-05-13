3ca46f487331161b48e28a4beebf979b7ccc0ea9
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2025-05-13 20:29:40 +0700

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,11 @@
 export default function DashboardPage() {
   return (
-    <main className="max-w-[100vw] mx-auto py-10 px-4">
-      <h1 className="text-2xl font-bold mb-6 text-center">Dashboard</h1>
-      {/* <iframe
-        title="abc"
-        width="1440"
-        height="800"
-        src="https://app.powerbi.com/reportEmbed?reportId=b275b614-a1e0-4482-bc15-5a94bb8b03a1&autoAuth=true&ctid=e7572e92-7aee-4713-a3c4-ba64888ad45f&pageNavigationVisible=false"
-        frameBorder="0"
-        allowFullScreen
-      ></iframe> */}
-
+    <main className="w-screen h-screen overflow-hidden">
       <iframe
         title="pb"
-        width="1440"
-        height="800"
         src="https://app.powerbi.com/reportEmbed?reportId=96a3bcbf-9368-4c38-9e60-540667b6b27e&autoAuth=true&ctid=e7572e92-7aee-4713-a3c4-ba64888ad45f&pageNavigationVisible=false"
         frameBorder="0"
+        className="w-[80vw] h-full border-none"
         allowFullScreen
       ></iframe>
     </main>
