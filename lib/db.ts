b56b6c86969222525dc969c7265ad2ff3bc04dc3
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2025-05-17 18:05:25 +0700

    update

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: '12345678',
-  database: 'ecom',
+  host: process.env.MYSQL_HOST || '127.0.0.1',
+  port: Number(process.env.MYSQL_PORT) || 3306,
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '12345678',
+  database: process.env.MYSQL_DATABASE || 'ecom',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
